667b46722a6d71a106b5c1c05040ec4fba4958db
kawase motoo <kawa@debian10LB.localdomain>
2020-04-21 14:49:07 +0900

    ver 0.2

--- pcrcov.py
+++ pcrcov.py
@@ -4,11 +4,20 @@ import pandas as pd
 import matplotlib.pyplot as plt
 from sklearn.kernel_ridge import KernelRidge as knlr
 
-df = pd.read_csv('T01.csv')
+df = pd.read_csv('summary.csv')
 va = df.values
-print(va.shape)
-x=va[:,0].reshape(-1,1)
-y=va[:,1].reshape(-1,1)
+skip=20
+wid=7
+yy = np.diff(va[skip:,3])
+xx = np.diff(va[skip:,4])
+n,=yy.shape
+xt=np.array([])
+yt=np.array([])
+for i in range(wid,n):
+    xt = np.append(xt,np.mean(xx[i-wid:i]))
+    yt = np.append(yt,np.mean(yy[i-wid:i]))
+x=xt.reshape(-1,1)
+y=yt.reshape(-1,1)
 kr=knlr(alpha=1, kernel='polynomial')
 kr.fit(x,y)
 tx = np.linspace(500,5000,100)
